17c56685240bc920f4efad5c1f7684a232190ca7
schneidfriederike-svg <schneid.friederike@gmail.com>
2026-06-10 17:47:36 +0200

    Create _agenda.js

---
+++ api/_agenda.js
@@ -0,0 +1,17 @@
+const AGENDA = [
+  { name: "Welcome",                  presenter: "@Charlotte Schneid",   mins: 1,  type: "content" },
+  { name: "Carbon market update",     presenter: "@Carla Woydt",         mins: 7,  type: "content" },
+  { name: "Carbon market update",     presenter: "Q&A",                  mins: 5,  type: "qa"      },
+  { name: "General company update",   presenter: "Company Leadership",   mins: 7,  type: "content" },
+  { name: "General company update",   presenter: "Q&A",                  mins: 5,  type: "qa"      },
+  { name: "eNPS results & takeaways", presenter: "@Mona Simons",         mins: 20, type: "content" },
+  { name: "New quotation feature",    presenter: "@Kaique Paes",         mins: 7,  type: "content" },
+  { name: "New quotation feature",    presenter: "Q&A",                  mins: 2,  type: "qa"      },
+  { name: "Risk dashboard",           presenter: "@Noé Lopez",           mins: 7,  type: "content" },
+  { name: "Risk dashboard",           presenter: "Q&A",                  mins: 2,  type: "qa"      },
+  { name: "Supply dashboard",         presenter: "@Kilian Balks",        mins: 7,  type: "content" },
+  { name: "Supply dashboard",         presenter: "Q&A",                  mins: 2,  type: "qa"      },
+  { name: "New joiners",              presenter: "Managers",             mins: 3,  type: "content" },
+];
+
+module.exports = AGENDA;
