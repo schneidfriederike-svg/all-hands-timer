d347ebabb11e4de4d31772bf6a9c344349675969
schneidfriederike-svg <schneid.friederike@gmail.com>
2026-06-24 18:08:10 +0200

    Update _agenda.js

--- api/_agenda.js
+++ api/_agenda.js
@@ -1,9 +1,15 @@
 const AGENDA = [
-  { name: "Market update",          presenter: "@Carla Woydt",       mins: 5,  type: "content" },
-  { name: "Market update",          presenter: "Q&A",                mins: 5,  type: "qa"      },
-  { name: "General company update", presenter: "Company Leadership", mins: 5,  type: "content" },
-  { name: "General company update", presenter: "Q&A",                mins: 5,  type: "qa"      },
-  { name: "Content deepdive 1",     presenter: "XX",                 mins: 10, type: "content" },
-  { name: "Content deepdive 1",     presenter: "Q&A",                mins: 5,  type: "qa"      },
+  { name: "Market update",            presenter: "@Carla",       mins: 7,   type: "content" },
+  { name: "Market update",            presenter: "Q&A",          mins: 5,   type: "qa"      },
+  { name: "General company update",   presenter: "CLS",          mins: 5,   type: "content" },
+  { name: "General company update",   presenter: "Q&A",          mins: 5,   type: "qa"      },
+  { name: "Sales playbook",           presenter: "@André",       mins: 10,  type: "content" },
+  { name: "Sales playbook",           presenter: "Q&A",          mins: 5,   type: "qa"      },
+  { name: "Automation demo: Yago/Noé", presenter: "@Yago/Noé",   mins: 7.5, type: "content" },
+  { name: "Automation demo: Yago/Noé", presenter: "Q&A",         mins: 2.5, type: "qa"      },
+  { name: "Automation demo: Johannes", presenter: "@Johannes",   mins: 7.5, type: "content" },
+  { name: "Automation demo: Johannes", presenter: "Q&A",         mins: 2.5, type: "qa"      },
+  { name: "Updates from People team", presenter: "@Mona",        mins: 5,   type: "content" },
+  { name: "New joiners",              presenter: "Managers",     mins: 3,   type: "content" },
 ];
 module.exports = AGENDA;
